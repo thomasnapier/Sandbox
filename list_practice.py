77d8e780fec53630b5a0c2b899d4243562978345
thomasnapier <thomas.napier@my.jcu.edu.au>
2017-08-14 09:54:49 +1000

    Update list practice

--- list_practice.py
+++ list_practice.py
@@ -1,6 +1,10 @@
-scores = []
-score = int(input("Score: "))
-while score >= 0:
-    scores.append(score)
+try:
+    scores = []
     score = int(input("Score: "))
-print("Your highest score is", max(scores))
+    while score >= 0:
+        scores.append(score)
+        score = int(input("Score: "))
+    print("Your highest score is", max(scores))
+
+except ValueError:
+    print("Enter in some real values first")
